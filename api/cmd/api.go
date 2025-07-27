403cb599b152a94a3f6898c332c325d3705b7cc6
Robert076 <robertberes06@gmail.com>
2025-07-27 09:05:11 +0300

    run server

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/Robert076/logger2.git/api/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.New()
-	r.POST("/", func(c *gin.Context) {
-		// checks + write to db
-	})
+	r.POST("/", handlers.HandlerPost)
+	r.Run(":8080")
 }
